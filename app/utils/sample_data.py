d0309cb761b7824b8e7ed0be30eeaacb081c0913
stds58 <stds58@gmail.com>
2025-10-03 23:28:09 +0300

    fix: refactoring data

--- app/utils/sample_data.py
+++ app/utils/sample_data.py
@@ -5,7 +5,7 @@ BUSINESS_ELEMENTS_DATA = [
         {"name": "order", "description": "Управление заказами"},
         {"name": "category", "description": "Управление категориями"},
         {"name": "access_rule", "description": "Управление правами доступа"},
-        {"name": "role", "description": "Управление ролями"},
+        {"name": "user_roles", "description": "Управление ролями"},
     ]
 
 ROLES_DATA = [
@@ -72,7 +72,7 @@ ACCESS_RULES_DATA = [
     },
     {
         "role_name": "admin",
-        "businesselement_name": "role",
+        "businesselement_name": "user_roles",
         "read_permission": True,
         "read_all_permission": True,
         "create_permission": True,
@@ -87,10 +87,10 @@ ACCESS_RULES_DATA = [
         "businesselement_name": "user",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
     {
@@ -98,10 +98,10 @@ ACCESS_RULES_DATA = [
         "businesselement_name": "product",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
     {
@@ -109,10 +109,10 @@ ACCESS_RULES_DATA = [
         "businesselement_name": "order",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
     {
@@ -120,10 +120,10 @@ ACCESS_RULES_DATA = [
         "businesselement_name": "category",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
     {
@@ -131,21 +131,21 @@ ACCESS_RULES_DATA = [
         "businesselement_name": "access_rule",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
     {
         "role_name": "manager",
-        "businesselement_name": "role",
+        "businesselement_name": "user_roles",
         "read_permission": True,
         "read_all_permission": False,
-        "create_permission": False,
-        "update_permission": False,
+        "create_permission": True,
+        "update_permission": True,
         "update_all_permission": False,
-        "delete_permission": False,
+        "delete_permission": True,
         "delete_all_permission": False
     },
 
@@ -206,7 +206,7 @@ ACCESS_RULES_DATA = [
     },
     {
         "role_name": "user",
-        "businesselement_name": "role",
+        "businesselement_name": "user_roles",
         "read_permission": True,
         "read_all_permission": False,
         "create_permission": False,
@@ -220,14 +220,21 @@ ACCESS_RULES_DATA = [
 USERS_DATA = [
     {
         "email": "admin@example.com",
-        "password": "admin123",
+        "password": "admin@example.com",
         "first_name": "Админ",
         "last_name": "Системный",
         "role_names": ["admin"]
     },
     {
+        "email": "manager@example.com",
+        "password": "manager@example.com",
+        "first_name": "manager",
+        "last_name": "manager",
+        "role_names": ["manager"]
+    },
+    {
         "email": "user@example.com",
-        "password": "user123",
+        "password": "user@example.com",
         "first_name": "Обычный",
         "last_name": "Пользователь",
         "role_names": ["user"]
