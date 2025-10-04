5fb0dced801cac53162edc8d3cb885f32c5fe1cf
stds58 <stds58@gmail.com>
2025-10-04 14:43:57 +0300

    fix: products

--- app/services/product.py
+++ app/services/product.py
@@ -1,23 +1,37 @@
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.crud.product import ProductDAO
-from app.schemas.product import SchemaProductCreate, SchemaProductFilter, SchemaProductPatch
+from app.schemas.base import PaginationParams
+from app.schemas.product import (
+    SchemaProductCreate,
+    SchemaProductFilter,
+    SchemaProductPatch,
+)
 from app.schemas.permission import AccessContext
 from app.exceptions.base import PermissionDenied
 
 
-async def find_many_product(access: AccessContext, filters: SchemaProductFilter, session: AsyncSession):
+async def find_many_product(
+    access: AccessContext,
+    filters: SchemaProductFilter,
+    session: AsyncSession,
+    pagination: PaginationParams,
+):
     if "read_all_permission" in access.permissions:
-        return await ProductDAO.find_many(filters=filters, session=session)
+        return await ProductDAO.find_many(
+            filters=filters, session=session, pagination=pagination
+        )
     if "read_permission" in access.permissions:
-        return await ProductDAO.find_many(filters=filters, session=session)
-    raise PermissionDenied(custom_detail="Missing read or read_all permission on product")
+        return await ProductDAO.find_many(
+            filters=filters, session=session, pagination=pagination
+        )
+    raise PermissionDenied(
+        custom_detail="Missing read or read_all permission on product"
+    )
 
 
 async def add_one_product(
-        access: AccessContext,
-        data: SchemaProductCreate,
-        session: AsyncSession
+    access: AccessContext, data: SchemaProductCreate, session: AsyncSession
 ):
     if "create_permission" in access.permissions:
         values_dict = data.model_dump(exclude_unset=True)
@@ -26,26 +40,32 @@ async def add_one_product(
 
 
 async def update_one_product(
-        access: AccessContext,
-        data: SchemaProductPatch,
-        session: AsyncSession,
-        product_id: UUID
+    access: AccessContext,
+    data: SchemaProductPatch,
+    session: AsyncSession,
+    product_id: UUID,
 ):
     filters_dict = data.model_dump(exclude_unset=True)
     if "update_all_permission" in access.permissions:
-        return await ProductDAO.update_one(model_id=product_id, session=session, values=filters_dict)
+        return await ProductDAO.update_one(
+            model_id=product_id, session=session, values=filters_dict
+        )
     if "update_permission" in access.permissions:
-        return await ProductDAO.update_one(model_id=product_id, session=session, values=filters_dict)
-    raise PermissionDenied(custom_detail="Missing update or update_all permission on product")
+        return await ProductDAO.update_one(
+            model_id=product_id, session=session, values=filters_dict
+        )
+    raise PermissionDenied(
+        custom_detail="Missing update or update_all permission on product"
+    )
 
 
 async def delete_one_product(
-        access: AccessContext,
-        session: AsyncSession,
-        product_id: UUID
+    access: AccessContext, session: AsyncSession, product_id: UUID
 ):
     if "delete_all_permission" in access.permissions:
         return await ProductDAO.delete_one_by_id(model_id=product_id, session=session)
     if "delete_permission" in access.permissions:
         return await ProductDAO.delete_one_by_id(model_id=product_id, session=session)
-    raise PermissionDenied(custom_detail="Missing delete or delete_all permission on product")
+    raise PermissionDenied(
+        custom_detail="Missing delete or delete_all permission on product"
+    )
