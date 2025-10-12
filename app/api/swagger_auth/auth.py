7e0b5fe7b5a099ee462b6a83decbdefaaea5fba3
stds58 <stds58@gmail.com>
2025-10-12 19:49:54 +0300

    fix: auth loging

--- app/api/swagger_auth/auth.py
+++ app/api/swagger_auth/auth.py
@@ -1,12 +1,18 @@
+import structlog
+from uuid import uuid4
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.permission import AccessContext
 from app.schemas.token import Token
+from app.schemas.user import SchemaUserLogin
 from app.services.auth_service import AuthService
-from app.services.user import authenticate_user_swagger
+from app.services.user import authenticate_user_swagger, get_user_by_email
 from app.dependencies.get_db import connection
 
+
+logger = structlog.get_logger()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/swaggerlogin")
 
@@ -18,7 +24,19 @@ async def swaggerlogin_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     session: AsyncSession = Depends(connection(isolation_level="READ COMMITTED")),
 ) -> Token:
-    return await authenticate_user_swagger(user_in=form_data, session=session)
+    filters = SchemaUserLogin(
+        email=form_data.username,
+        password="*****"
+    )
+    fake_uuid = uuid4()
+    access = AccessContext(user_id=fake_uuid, permissions=["read_all_permission"])
+    user = await get_user_by_email(access=access, email=form_data.username, session=session)
+
+    logger.info("Login swagger user", user_id=user.id, filters=filters)
+    obj = await authenticate_user_swagger(user_in=form_data, session=session)
+    logger.info("Logined swagger user", user_id=user.id, filters=filters)
+
+    return obj
 
 
 @swagger_router.post("/logout")
